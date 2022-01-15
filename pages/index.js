34c328681613e45742a6b1b687ed5fa6bf8d3c43
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-01-14 23:53:42 -0500

    refactor: displaying address data using covalent

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Header from './components/header';
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const [address, setAddress] = useState('');
+  const [covalentData, setCovalentData] = useState([]);
+
+  const getData = async () => {
+    const covalent = "https://api.covalenthq.com/v1/1/address/" + address + "/transactions_v2/?quote-currency=USD&format=JSON&block-signed-at-asc=false&no-logs=false&key=" + process.env.NEXT_PUBLIC_COVALENT_API_KEY;
+    const covalentRes = await axios.get(covalent);
+    setCovalentData(covalentRes.data.data);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +27,15 @@ export default function Home() {
       <h2>{`Top grossing NFT's`}</h2>
       <div className={styles.grid}>
         display top grossing nft's here 
+      </div>
+      <div>
+        <form>
+          <input type="text" placeholder="address" value={address} onChange={(e) => setAddress(e.target.value)} />
+          <button onClick={(e) => {e.preventDefault(); getData();}}>view my data</button>
+        </form>
+      </div>
+      <div>
+          {JSON.stringify(covalentData)}
       </div>
     </div>
   )
