33848b0e3f2281de4837f1349a8b3201926797f3
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-01-14 22:49:37 -0500

    refactor: centered mint title text

---
+++ pages/mint.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Home.module.css';
+
+export default function Mint() {
+    return (
+        <div>
+            <h1 className={styles['center-text']}>Mint</h1>
+        </div>
+    );
+}
